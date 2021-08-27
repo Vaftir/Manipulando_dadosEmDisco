fd4aecad1361ef0505f43e65d7216cc6f6b543ee
Vaftir <yagoassisfaria@gmail.com>
2021-08-26 23:39:10 -0300

    negando o teste invalido

--- src/manipulandoArquivos/Livro.java
+++ src/manipulandoArquivos/Livro.java
@@ -1,72 +1,63 @@
 package manipulandoArquivos;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.DataInputStream;
-import java.io.FileInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
-
 
 public class Livro {
 	protected int idLivro;
 	protected String titulo;
 	protected String nome;
 	protected float preco;
-	
+
 	DecimalFormat df = new DecimalFormat("#,##0.00");
-	
-	
+
 	public Livro(int i, String t, String a, float p) {
-		idLivro =i;
+		idLivro = i;
 		titulo = t;
 		nome = a;
 		preco = p;
 	}
-	
+
 	public Livro() {
-		idLivro =-1;
+		idLivro = -1;
 		titulo = "";
 		nome = "";
 		preco = 0F;
 	}
 
-	
-	
 	public String toString() {
-		
-		return "\nID: " + idLivro +
-				"\nTitulo: "+titulo+
-				"\nNome: " + nome +
-				"\nPreco: R$"+ df.format(preco);
+
+		return "\nID: " + idLivro + "\nTitulo: " + titulo + "\nNome: " + nome + "\nPreco: R$" + df.format(preco);
 	}
-	
-	
+
 	public byte[] toByteArray() throws IOException { /// cria um vetor de bytes e escreve no arquivo
-		
+
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream das = new DataOutputStream(baos);
-		
+
 		das.writeInt(idLivro);
 		das.writeUTF(titulo);
 		das.writeUTF(nome);
 		das.writeFloat(preco);
-		
+
 		return baos.toByteArray();
-		
+
 	}
-	
-	
-	public void fromByteArray(byte[] ba) throws IOException {///CARREAGA OS VALORES DOS ARQUIVOS NO VETOR
-		
+
+	public void fromByteArray(byte[] ba) throws IOException {/// CARREAGA OS VALORES DOS ARQUIVOS NO VETOR
+
 		ByteArrayInputStream bais = new ByteArrayInputStream(ba);
 		DataInputStream dis = new DataInputStream(bais);
-		
+
 		idLivro = dis.readInt();
 		titulo = dis.readUTF();
 		nome = dis.readUTF();
 		preco = dis.readFloat();
-		
+
 	}
-	
+
 }
